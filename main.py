f8af879e280b6bbee6a34bf1f0b625e97e28fc56
ahegde3 <ahegde3@outlook.com>
2024-06-25 20:49:36 -0400

    sheet data updated with rss feed

--- main.py
+++ main.py
@@ -2,26 +2,42 @@ import feedparser
 import json
 from dotenv import load_dotenv
 import os
+import datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-def fetchRssData():
-    # URL of the RSS feed
-    rss_url = "https://www.goodreads.com/review/list_rss/70445162?key=qg0XmblgluNyknWOzUaB4y2ApKPhq64rhV6MNScwws7hKg3B&shelf=read"
+load_dotenv()
 
-    # Parse the RSS feed
-    feed = feedparser.parse(rss_url)
-    print(feed.entries[0])
+# Constants
+RSS_URL = "https://www.goodreads.com/review/list_rss/70445162?key=qg0XmblgluNyknWOzUaB4y2ApKPhq64rhV6MNScwws7hKg3B&shelf=read"
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'service-account.json'
+RANGE_NAME = 'Sheet1!A1:ZZ'
+SPREADSHEET_ID = os.environ.get("SPREADSHEET_ID")
 
-    # Convert the feed to a JSON object
-    json_data = json.dumps(feed.entries, indent=4)
-    # Convert the JSON string back to a Python object
-    data = json.loads(json_data)
 
-    # Print the JSON output
-    print(len(data))
+def fetch_rss_data():
+    """Fetches and parses data from an RSS feed."""
 
-def inititalizeGoogleSheet():
+    try:
+
+        # Parse the RSS feed
+        feed = feedparser.parse(RSS_URL)
+
+        # Convert the feed to a JSON object
+        json_data = json.dumps(feed.entries, indent=4)
+        # Convert the JSON string back to a Python object
+        data = json.loads(json_data)
+
+        return data
+    
+    except Exception as e:
+        print(f"Error fetching RSS data: {e}")
+        return None
+
+def initialize_google_sheet():
+    """Initializes a Google Sheets API client and reads data from a specified sheet."""
+
     print("Initializing Google Sheet")
 
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -34,24 +50,45 @@ def inititalizeGoogleSheet():
     # Build the Sheets API client
     service = build('sheets', 'v4', credentials=credentials)
 
+    return service
+
+
+def insert_data_into_sheet(sheetService, data):
+    """Inserts data into a Google Sheet."""
+
+    print("Inserting data into Google Sheet")
+
     # Replace with your spreadsheet ID and range
-    spreadsheet_id = os.environ.get("SPREADSHEET_ID")
-    print(spreadsheet_id)
-    
     range_name = 'Sheet1!A1:ZZ'
 
-    # Read data from the sheet
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id, range=range_name
-    ).execute()
+    # Convert the data to a 2D list
+    # Define headers
+    headers = ["Title", "Author", "Read At", "Rating"]
+    values = [headers]
+    for item in data:
 
-    print(result)
+        if item['user_read_at'] == "":
+            read_at = ""
+        else :
+            read_at = datetime.datetime.strptime(item['user_read_at'],"%a, %d %b %Y %H:%M:%S %z").strftime('%d/%B/%Y')    
+        values.append([item["title"], item['author_name'],read_at ,item['user_rating']])
+
+  
+
+    # Update the Google Sheet
+    request = sheetService.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID, range=range_name,
+        valueInputOption="RAW", body={"values": values}
+    )
+    response = request.execute()
+
 
 if __name__ == "__main__":
     
 
    #Load environment variables from .env file
     load_dotenv()
+    sheetService = initialize_google_sheet() 
+    data = fetch_rss_data()
 
-    # fetchRssData()
-    inititalizeGoogleSheet() 
+    insert_data_into_sheet(sheetService, data)
